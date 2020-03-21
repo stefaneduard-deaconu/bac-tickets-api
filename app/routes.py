37c11bc77c80be75ee6c47496abba2971e3c3d04
stefaneduard-deaconu <stefaneduard.deaconu@gmail.com>
2020-03-21 17:50:43 +0200

    the POST requeste to /login now works

--- app/routes.py
+++ app/routes.py
@@ -1,11 +1,14 @@
-from flask import render_template
+from flask import render_template, flash, redirect
 from app import app
 from app.forms import LoginForm
 
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        flash(f'Login requested for user {form.username.data}, remember_me={form.remember_me.data}')
+        return redirect('/index')
     return render_template('login.html', form=form)
 
 

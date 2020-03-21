7e7d971ed1823cc00b80d504872f4aadaec28fce
stefaneduard-deaconu <stefaneduard.deaconu@gmail.com>
2020-03-21 17:43:58 +0200

    added flask-ftw and GET route for login

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,21 @@
+from flask import render_template
 from app import app
+from app.forms import LoginForm
 
-@app.route('/', methods=["GET"])
-def root():
-    return '''
-    <h1> Welcome home </h1>
-    '''
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html', form=form)
+
+
+@app.route('/signup')
+def signup():
+    pass  # TODO
+
+
+@app.route('/')
+@app.route('/index')
+def index():
+    user = {'name': 'Stefan'}
+    return render_template('index.html', user=user)
